db4b2aa058599ac7cb92fba59890e69c6f1180c8
Leigh Pauls <leigh.pauls@gmail.com>
2014-04-22 22:01:23 -0500

    Code and data used to generate results for paper

--- decision_tree.py
+++ decision_tree.py
@@ -10,9 +10,9 @@ from sklearn.ensemble import GradientBoostingClassifier
 TRAINING_RATIO = 0.7
 
 if __name__=='__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print "usage:", sys.argv[0], \
-            '<twss-json-file> <tfln-json-file>'
+            '<twss-json-file> <tfln-json-file> <output-csv'
         exit(-1)
     all_twss_words, twss_word_sets = j2t.twss_json_to_word_usage(sys.argv[1])
     all_tfln_words, tfln_word_sets = j2t.tfln_json_to_word_usage(sys.argv[2])
@@ -45,33 +45,52 @@ if __name__=='__main__':
 
     # print 'training:', len(training_inputs), 'testing:', len(testing_inputs)
 
-    clf = RandomForestClassifier(n_estimators=200, n_jobs=7)
-    # n_estimators = 470
-    # max_features = 2
-    # clf = ExtraTreesClassifier(
-    #     n_jobs=6,
-    #     n_estimators=n_estimators,
-    #     max_features=max_features)
-    # print "n_estimators: ", n_estimators
-    # print "max_features: ", max_features
+    results = []
+    for max_features in [1, 5, 10, 15, 20, 30, 50]:
+        # clf = RandomForestClassifier(n_estimators=200, n_jobs=7)
+        n_estimators = 100
+        # max_features = 12
+        clf = ExtraTreesClassifier(
+            n_jobs=6,
+            n_estimators=n_estimators,
+            max_features=max_features)
+        # print "n_estimators: ", n_estimators
+        # print "max_features: ", max_features
 
-    clf.fit(training_inputs, training_outputs)
-    predicted_outputs = clf.predict(testing_inputs)
-    probs = clf.predict_proba(testing_inputs)
+        clf.fit(training_inputs, training_outputs)
+        predicted_outputs = clf.predict(testing_inputs)
+        probs = clf.predict_proba(testing_inputs)
 
-    print "'threshold','precision','recall'"
-    # clf = tree.DecisionTreeClassifier()
-    for THRESHOLD in xrange(0.1, 0.9, 0.05):
+        # print "'threshold','recall','precision'"
+        # clf = tree.DecisionTreeClassifier()
+        estimator_result = {}
+        estimator_result["line"] = []
+        estimator_result["max_features"] = max_features
+        for THRESHOLD in (x * 0.05 for x in xrange(1, 20)):
+            cm = [[0, 0], [0, 0]];
+            # THRESHOLD = 0.7
+            for i in xrange(len(testing_outputs)):
+                prediction = 1 if probs[i][1] > THRESHOLD else 0
+                cm[testing_outputs[i]][prediction] += 1
+            result = {}
+            result["threshold"] = THRESHOLD
+            result["precision"] = (float(cm[1][1]) / float(cm[0][1] + cm[1][1]))
+            result["recall"] = (float(cm[1][1]) / float(cm[1][0] + cm[1][1]))
+            estimator_result["line"].append(result)
+        results.append(estimator_result)
 
-        cm = [[0, 0], [0, 0]];
-        # THRESHOLD = 0.7
-        for i in xrange(len(testing_outputs)):
-            prediction = 1 if probs[i][1] > THRESHOLD else 0
-            cm[testing_outputs[i]][prediction] += 1
 
-        print cm
-        print THRESHOLD, ",", \
-            (float(cm[1][1]) / float(cm[0][1] + cm[1][1])), ",", \
-            (float(cm[1][1]) / float(cm[1][0] + cm[1][1]))
-        # print "Precision:", (float(cm[1][1]) / float(cm[0][1] + cm[1][1]))
-        # print "Recall:", (float(cm[1][1]) / float(cm[1][0] + cm[1][1]))
+    with open(sys.argv[3], "w") as f:
+        # print the title line
+        for estimator_result in results:
+            base_string = 'res_' + str(estimator_result["max_features"]) + "_features_"
+            f.write(base_string + "recall,")
+            f.write(base_string + "precision,")
+        f.write("\n")
+
+        # write the results
+        for i in xrange(len(results[0]["line"])):
+            for estimator_result in results:
+                f.write(str(estimator_result["line"][i]["recall"]) + ",")
+                f.write(str(estimator_result["line"][i]["precision"]) + ",")
+            f.write("\n")
